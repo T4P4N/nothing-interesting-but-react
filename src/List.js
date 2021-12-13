396de1116ae4e2c6bf11b05b810f0f4f382c60a2
T4P4N <tapank415@gmail.com>
2021-12-13 12:43:35 +0530

    added x btn!

--- src/List.js
+++ src/List.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './style.css';
-import { ChevronUp, User } from 'react-feather';
+import { ChevronUp, User, X } from 'react-feather';
 
 const Item = ({ item, onRemoveItem }) => {
   const handleRemoveItem = () => {
@@ -10,19 +10,18 @@ const Item = ({ item, onRemoveItem }) => {
   return (
     <div key={item.objectID} className="posts">
       <h4 className="title">
+        <button type="button" onClick={handleRemoveItem}>
+          <X height="15px" width="15px" className="x-btn" />
+        </button>
         <a href={item.url}>{item.title}</a>
       </h4>
+
       <p className="author">
         <User size={14} /> {item.author}
       </p>
       <p className="points">
         <ChevronUp /> {item.points}
       </p>
-      {/* <td>
-        <button type="button" onClick={handleRemoveItem}>
-          X
-        </button>
-      </td> */}
     </div>
   );
 };
