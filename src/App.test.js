7c8126bdb2491898cd3139e813ebd2d223b883cd
T4P4N <tapank415@gmail.com>
2021-12-20 18:59:31 +0530

    Things are getting complicated here in stackblitz might have switch to codesanbox.io

--- src/App.test.js
+++ src/App.test.js
@@ -1,40 +1,40 @@
-describe('something truthy', () => {
-  it('true to be true', () => {
+describe("something truthy", () => {
+  it("true to be true", () => {
     expect(true).toBe(true);
   });
 });
 
 // test suite
 
-describe('Truthy and falsy', () => {
+describe("Truthy and falsy", () => {
   // test case
-  it('true to be true', () => {
+  it("true to be true", () => {
     // test assertation
     expect(false).toBe(false);
   });
 });
 
-import React from 'react';
-import renderer from 'react-test-renderer';
+import React from "react";
+import renderer from "react-test-renderer";
 
-import App, { Item, List, SearchForm, InputWithLabel } from './App';
+import App, { Item, List, SearchForm, InputWithLabel } from "./App";
 
 // item test cases
-describe('Item', () => {
+describe("Item", () => {
   const item = {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
+    title: "React",
+    url: "https://reactjs.org",
+    author: "Jordan Walke",
     num_comments: 3,
     points: 4,
-    objectID: 0,
+    objectID: 0
   };
 
-  it('renders all properties', () => {
+  it("renders all properties", () => {
     const component = renderer.create(<Item item={item} />);
 
-    expect(component.root.findAllByType('p')[0].props.children[2]).toEqual(
-      'Jordan Walke'
+    expect(component.root.findAllByProps({ children: "Jordan Walke" })).toEqual(
+      "1"
     );
   });
 });
