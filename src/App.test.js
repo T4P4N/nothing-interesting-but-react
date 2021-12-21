e6f9dbbad8b5f07951797136cd035065509e6d9d
T4P4N <tapank415@gmail.com>
2021-12-21 12:26:12 +0530

    todo: fix tests according to convention.

--- src/App.test.js
+++ src/App.test.js
@@ -1,40 +1,47 @@
-describe("something truthy", () => {
-  it("true to be true", () => {
+import React from 'react';
+import renderer from 'react-test-renderer';
+import App, { Item, List, SearchForm, InputWithLabel } from './App';
+
+describe('something truthy', () => {
+  it('true to be true', () => {
     expect(true).toBe(true);
   });
 });
 
 // test suite
 
-describe("Truthy and falsy", () => {
+describe('Truthy and falsy', () => {
   // test case
-  it("true to be true", () => {
+  it('true to be true', () => {
     // test assertation
     expect(false).toBe(false);
   });
 });
 
-import React from "react";
-import renderer from "react-test-renderer";
-
-import App, { Item, List, SearchForm, InputWithLabel } from "./App";
-
 // item test cases
-describe("Item", () => {
+describe('Item', () => {
   const item = {
-    title: "React",
-    url: "https://reactjs.org",
-    author: "Jordan Walke",
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
     num_comments: 3,
     points: 4,
-    objectID: 0
+    objectID: 0,
   };
 
-  it("renders all properties", () => {
+  it('renders all properties', () => {
     const component = renderer.create(<Item item={item} />);
-
-    expect(component.root.findAllByProps({ children: "Jordan Walke" })).toEqual(
-      "1"
+    // author
+    expect(component.root.findAllByType('p')[0].props.children[2]).toEqual(
+      'Jordan Walke'
     );
+    // title
+    expect(component.root.findAllByType('a')[0].props.children).toEqual(
+      'React'
+    );
+    // points
+    expect(component.root.findAllByType('p')[1].props.children[2]).toEqual(4);
   });
+
+  // This is all shit we should only data-testid to get elements
 });
