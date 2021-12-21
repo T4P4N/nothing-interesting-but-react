9192211529b8092610b4a17afcecf79d94ff50fc
T4P4N <tapank415@gmail.com>
2021-12-21 19:51:55 +0530

    added new files manually! :(

---
+++ nothing-interest-but-react/src/App.tsx
@@ -0,0 +1,285 @@
+import React from 'react';
+import axios from 'axios';
+import { act } from 'react-dom/test-utils';
+import { ChevronUp, User, X, Search, Heart } from 'react-feather';
+
+const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
+
+const useSemiPersistentState = (
+  key: string,
+  initialState: string
+): [string, (newValue: string) => void] => {
+  const [value, setValue] = React.useState(
+    localStorage.getItem(key) || initialState
+  );
+
+  React.useEffect(() => {
+    localStorage.setItem(key, value);
+  }, [value, key]);
+
+  return [value, setValue];
+};
+
+type Story = {
+  objectID: string;
+  url: string;
+  title: string;
+  author: string;
+  num_comments: number;
+  points: number;
+};
+
+type Stories = Array<Story>;
+
+type StoriesState = {
+  data: Stories;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+interface StoriesFetchInitAction {
+  type: 'STORIES_FETCH_INIT';
+}
+
+interface StoriesFetchSuccessAction {
+  type: 'STORIES_FETCH_SUCCESS';
+  payload: Stories;
+}
+
+interface StoriesFetchFailureAction {
+  type: 'STORIES_FETCH_FAILURE';
+}
+
+interface StoriesRemoveAction {
+  type: 'REMOVE_STORY';
+  payload: Story;
+}
+
+type StoriesAction =
+  | StoriesFetchInitAction
+  | StoriesFetchSuccessAction
+  | StoriesFetchFailureAction
+  | StoriesRemoveAction;
+
+const storiesReducer = (state: StoriesState, action: StoriesAction) => {
+  switch (action.type) {
+    case 'STORIES_FETCH_INIT':
+      return {
+        ...state,
+        isLoading: true,
+        isError: false,
+      };
+    case 'STORIES_FETCH_SUCCESS':
+      return {
+        ...state,
+        isLoading: false,
+        isError: false,
+        data: action.payload,
+      };
+    case 'STORIES_FETCH_FAILURE':
+      return {
+        ...state,
+        isLoading: false,
+        isError: true,
+      };
+
+    case 'REMOVE_STORY':
+      return {
+        ...state,
+        data: state.data.filter(
+          (story) => action.payload.objectID !== story.objectID
+        ),
+      };
+    default:
+      throw new Error();
+  }
+};
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useSemiPersistentState('state', 'React');
+
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
+
+  const [stories, dispatchStories] = React.useReducer(storiesReducer, {
+    data: [],
+    isLoading: false,
+    isError: false,
+  });
+
+  const handleFetchStories = React.useCallback(async () => {
+    dispatchStories({ type: 'STORIES_FETCH_INIT' });
+
+    try {
+      const result = await axios.get(url);
+
+      dispatchStories({
+        type: 'STORIES_FETCH_SUCCESS',
+        payload: result.data.hits,
+      });
+    } catch {
+      dispatchStories({
+        type: 'STORIES_FETCH_FAILURE',
+      });
+    }
+  }, [url]);
+
+  React.useEffect(() => {
+    handleFetchStories();
+  }, [handleFetchStories]);
+
+  const handleRemoveStory = (item: Story) => {
+    dispatchStories({
+      type: 'REMOVE_STORY',
+      payload: item,
+    });
+  };
+
+  const handleSearchInput = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
+
+    event.preventDefault();
+  };
+
+  return (
+    <div className="container">
+      <h1 className="main-head">Hacker News</h1>
+
+      <SearchForm
+        searchTerm={searchTerm}
+        onSearchInput={handleSearchInput}
+        onSearchSubmit={handleSearchSubmit}
+      />
+
+      <br />
+
+      {stories.isError && <p>Something went wrong</p>}
+
+      {stories.isLoading ? (
+        <div className="lds-dual-ring"></div>
+      ) : (
+        <div className="post-container">
+          <List data={stories.data} onRemoveItem={handleRemoveStory} />
+        </div>
+      )}
+      <footer>
+        <h3 className="footer">
+          Made with <Heart /> by T4P4N
+        </h3>
+      </footer>
+    </div>
+  );
+};
+
+type SearchFormProps = {
+  searchTerm: string;
+  onSearchInput: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onSearchSubmit: (event: React.FormEvent<HTMLFormElement>) => void;
+  // what is void?????
+};
+
+const SearchForm = ({
+  searchTerm,
+  onSearchInput,
+  onSearchSubmit,
+}: SearchFormProps) => (
+  <form onSubmit={onSearchSubmit}>
+    <InputWithLabel
+      id="search"
+      value={searchTerm}
+      isFocused
+      onInputChange={onSearchInput}
+    >
+      {/* <strong>Search:</strong> */}
+    </InputWithLabel>
+    <button type="submit" disabled={!searchTerm} className="search-btn">
+      <Search height="20px" width="20px" className="search-btn" />
+    </button>
+  </form>
+);
+
+type InputWithLabelProps = {
+  id: string;
+  value: string;
+  type?: string;
+  onInputChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isFocused?: boolean;
+  children: React.ReactNode;
+};
+
+const InputWithLabel = ({
+  id,
+  value,
+  type = 'text',
+  onInputChange,
+  isFocused,
+  children,
+}: InputWithLabelProps) => {
+  const inputRef = React.useRef<HTMLInputElement>(null!);
+
+  React.useEffect(() => {
+    if (isFocused && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [isFocused]);
+  // To fix these swiggly lines i think we gotta setup tsconfig
+  return (
+    <>
+      <label htmlFor={id}>{children}</label>
+      &nbsp;
+      <input id={id} type={type} value={value} onChange={onInputChange} />
+    </>
+  );
+};
+
+type ListProps = {
+  // list: Stories;
+  data: Stories;
+  onRemoveItem: (item: Story) => void;
+};
+
+const List = ({ data, onRemoveItem }: ListProps) => {
+  return (
+    <div>
+      {data.map((item) => (
+        <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem} />
+      ))}
+    </div>
+  );
+};
+
+type ItemProps = {
+  item: Story;
+  onRemoveItem: (item: Story) => void;
+};
+
+const Item = ({ item, onRemoveItem }: ItemProps) => {
+  const handleRemoveItem = () => {
+    onRemoveItem(item);
+    console.log('Removed --> ' + item.title);
+  };
+
+  return (
+    <div key={item.objectID} className="posts">
+      <h4 className="title">
+        <button type="button" onClick={handleRemoveItem}>
+          <X height="15px" width="15px" className="x-btn" />
+        </button>
+        <a href={item.url}>{item.title}</a>
+      </h4>
+
+      <p className="author">
+        <User size={14} /> {item.author}
+      </p>
+      <p className="points">
+        <ChevronUp /> {item.points}
+      </p>
+    </div>
+  );
+};
+
+export default App;
+export {SearchForm, List, Item, InputWithLabel}
---
+++ nothing-interest-but-react/src/index.tsx
@@ -0,0 +1,6 @@
+import { render } from "react-dom";
+import "/src/styles.css";
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
