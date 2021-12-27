c9b36c2be57ffed6e5ec0fc4fde98d32c6e1bb14
Tapan Kashyap <32695903+T4P4N@users.noreply.github.com>
2021-12-27 17:52:50 +0530

    no changes

--- src/App.tsx
+++ src/App.tsx
@@ -111,11 +111,12 @@ const getLastSearches = (urls) => {
   console.log(urls);
 
   // Somethings wrong here may found page 202?
+  // on fivevith time
   var fixme = urls
     // Slice it to last five ones but not including current one
-    // .slice(-5, -1)
-    .slice(-6)
-    .slice(0, -1)
+    .slice(-5, -1)
+    // .slice(-6)
+    // .slice(0, -1)
     .map((url) => extractSearchTerm(url));
 
   var fixed = uniq(fixme);
